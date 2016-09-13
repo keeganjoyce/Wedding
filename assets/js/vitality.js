720bf8ffaf13f809989ec6c182245464bad21917
keeganjoyce <keeganwj@gmail.com>
2016-09-13 15:55:36 +1000

    Yo bitch initial commit

---
+++ assets/js/vitality.js
@@ -0,0 +1,232 @@
+/*!
+ * Vitality v1.3.7 (http://themes.startbootstrap.com/vitality-v1.3.7)
+ * Copyright 2013-2016 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Owl Carousel Settings
+    $(".about-carousel").owlCarousel({
+        items: 3,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    });
+
+    $(".portfolio-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        autoHeight: true,
+        mouseDrag: false,
+        touchDrag: false,
+        transitionStyle: "fadeUp"
+    });
+
+    $(".testimonials-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: true,
+        autoHeight: true,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        transitionStyle: "backSlide"
+    });
+
+    $(".portfolio-gallery").owlCarousel({
+        items: 3,
+    });
+    
+    /* ======= RSVP Form (Dependent form field) ============ */
+    $('#cguests').on("change", function(){
+        
+        if ($(this).val() == "") {
+            $('.guestinfo-group').slideUp(); //hide
+            console.log('not selected');
+        } else if ($(this).val() == 'No Guest' ) {
+            $('.guestinfo-group').slideUp(); //hide
+            console.log('No guests');
+            $('#cguestinfo').val('No Guest'); //Pass data to the field so mailer.php can send the form.
+            
+        } else {
+            $('.guestinfo-group').slideDown(); //show
+            $('#cguestinfo').val(''); //Clear data
+            console.log('Has guest');
+        }
+
+       
+    });
+    
+/* ======= Countdown ========= */
+    // set the date we're counting down to
+    var target_date = new Date("December 22, 2016").getTime();
+     
+    // variables for time units
+    var days, hours, minutes, seconds;
+     
+    // get tag element
+    var countdown =  document.getElementById("countdown-box");
+    var days_span = document.createElement("SPAN");
+    days_span.className = 'days';
+    countdown.appendChild(days_span);
+    var hours_span = document.createElement("SPAN");
+    hours_span.className = 'hours';
+    countdown.appendChild(hours_span);
+    var minutes_span = document.createElement("SPAN");
+    minutes_span.className = 'minutes';
+    countdown.appendChild(minutes_span);
+    var secs_span = document.createElement("SPAN");
+    secs_span.className = 'secs';
+    countdown.appendChild(secs_span);
+     
+    // update the tag with id "countdown" every 1 second
+    setInterval(function () {
+     
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
+     
+        // do some time calculations
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
+         
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+         
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+         
+        // format countdown string + set tag value.
+        days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit script">Days</span>';
+        hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit script">Hrs</span>';
+        minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit script">Mins</span>';
+        secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit script">Secs</span>';
+        
+      
+        //countdown.innerHTML = days + "d, " + hours + "h, "
+       // + minutes + "m, " + seconds + "s";  
+     
+    }, 1000);
+     
+
+    // Magnific Popup jQuery Lightbox Gallery Settings
+    $('.gallery-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    // Formstone Background - Video Background Settings
+    $("header.video").background({
+        source: {
+            poster: "assets/img/bg-mobile-fallback.jpg",
+            mp4: "assets/mp4/camera.mp4"
+        }
+    });
+
+    // Scrollspy: Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Portfolio Filtering Scripts & Hover Effect
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                // call the hover effect
+                onMixEnd: filterList.hoverEffect()
+            });
+
+        },
+
+        hoverEffect: function() {
+
+            // Simple parallax effect
+            $('#portfoliolist .portfolio').hover(
+                function() {
+                    $(this).find('.caption').stop().animate({
+                        bottom: 0
+                    }, 200, 'easeOutQuad');
+                    $(this).find('img').stop().animate({
+                        top: -20
+                    }, 300, 'easeOutQuad');
+                },
+                function() {
+                    $(this).find('.caption').stop().animate({
+                        bottom: -75
+                    }, 200, 'easeInQuad');
+                    $(this).find('img').stop().animate({
+                        top: 0
+                    }, 300, 'easeOutQuad');
+                }
+            );
+
+        }
+
+    };
+
+    filterList.init();
+
+})(jQuery); // End of use strict
+
+// Load WOW.js on non-touch devices
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop               
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+});
