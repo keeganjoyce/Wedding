f009def75916080006353174992b3bc273132fe2
Maddi Joyce <maddi@maddijoyce.com>
2016-10-06 17:47:04 +1100

    Add Carousel Slides.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,36 @@
 /* ======= Countdown ========= */
-    // set the date we're counting down to
-    var target_date = new Date("December 22, 2016").getTime();
+// set the date we're counting down to
+var target_date = new Date("December 22, 2016").getTime();
 
-    // variables for time units
-    var days, hours, minutes, seconds;
+// variables for time units
+var days, hours, minutes, seconds;
 
-    // update the tag with id "countdown" every 1 second
-    setInterval(function () {
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
 
-        days = parseInt(seconds_left / 86400);
-        seconds_left = seconds_left % 86400;
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
 
-        hours = parseInt(seconds_left / 3600);
-        seconds_left = seconds_left % 3600;
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
 
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
 
-        // format countdown string + set tag value.
-        $('#days').text(days);
-        $('#hours').text(hours);
-        $('#minutes').text(minutes);
-        $('#seconds').text(seconds);
-    }, 1000);
+    // format countdown string + set tag value.
+    $('#days').text(days);
+    $('#hours').text(hours);
+    $('#minutes').text(minutes);
+    $('#seconds').text(seconds);
+}, 1000);
+
+$('#carousel').on('slide.bs.carousel', function() {
+    $('.slide-indicator').removeClass('active');
+});
+
+$('#carousel').on('slid.bs.carousel', function() {
+    var id = $(this).find('.carousel-item.active').attr('id');
+    $('.slide-indicator[data-carousel="#' + id + '"]').addClass('active');
+});
